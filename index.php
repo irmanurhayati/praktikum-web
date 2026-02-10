8bb5a78d8a1255ee516fc248b234b36ba75fae28
irmanurhayati <irman4970@gmail.com>
2026-02-10 18:33:40 +0700

    Add basic HTML structure to index.php

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pertemuan 1</title>
+</head>
+<body>
+    <p>Assalamualaikum Pemrograman Web</p>
+    <br>
+    <h1>INI JUDUL</h1>
+    <h2>Bab 1 Laragon saya beda versi</h2>
+     <p>Nunggu video tutornya kak</p>
+</body>
+</html>
